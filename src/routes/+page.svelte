9f9f1a7d5c10ec7eef8b9d4c8e8e9a946fdddd23
TwanAsselbergs <twanasselbergs@outlook.com>
2024-06-04 15:50:39 +0200

    push

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+	let a = 1;
+</script>
+
+<h1 class="text-8xl">Brawlers {a}</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button
+	on:click={() => {
+		a++;
+	}}>increment</button
+>
