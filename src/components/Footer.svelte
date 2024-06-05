fbadb058b4b1facdfb81acdb0a30c47740ae493d
TwanAsselbergs <twanasselbergs@outlook.com>
2024-06-05 14:35:25 +0200

    footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,6 +1,35 @@
 <footer
-	style="background-color: #28A644; color: white; font-family: Arial, sans-serif;"
-	class="text-center py-3"
+	class="bg-green-600 text-white text-center py-3 font-sans font"
+	style="font-family: Arial, Helvetica, sans-serif;"
 >
-	<p>Copyright 2022. All rights reserved.</p>
+	<div class="flex justify-center items-center flex-row">
+		<div class="w-1/4 mt-5 mr-12">
+			<p class="font-bold text-white"><a href="/">Hogendijk Hoveniers</a></p>
+			<div style="height: 1px;" class="bg-white mt-1 w-5 mx-auto mb-0.5"></div>
+			<p>
+				Geen opdracht is te klein of te groot voor mijn hoveniersbedrijf! Alle soorten werkzaamheden
+				in en rond de tuin voer ik als hoveniersbedrijf met veel passie voor het vak uit.
+				<br /><br />
+				Zo kunt u er dan ook blindelings vanuit gaan dat uw tuin naar wens wordt opgeleverd, eerder zal
+				ik namelijk niet vertrekken. Vakmanschap, kwaliteit en service, dat is het handelsmerk van Hogendijk
+				Hoveniers.
+			</p>
+		</div>
+		<div class="mr-24">
+			<p class="font-bold text-white">Adres</p>
+			<div style="height: 1px;" class="bg-white mt-1 w-5 mx-auto mb-0.5"></div>
+			<p class="text-sm">Merellaan 343</p>
+			<p class="text-sm">2903 GH Capelle a/d IJssel</p>
+		</div>
+		<div>
+			<p class="font-bold text-white">Contact</p>
+			<div style="height: 1px;" class="bg-white mt-1 w-5 mx-auto mb-0.5"></div>
+			<p class="text-sm">Algemeen: 010 442 56 15</p>
+			<p class="text-sm">Albert Kasbergen: 06 470 20 960</p>
+			<p class="text-sm">E-mail: info@kasbergen-hoveniers.nl</p>
+		</div>
+	</div>
+	<div>
+		<p class="text-sm mt-10">&copy; 2024 Alle rechten voorbehouden aan Hogendijk Hoveniers</p>
+	</div>
 </footer>
