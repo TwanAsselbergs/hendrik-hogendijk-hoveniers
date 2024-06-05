3e44a8e87affdf6c6ff9188ca8a36a13d9d06a31
TwanAsselbergs <twanasselbergs@outlook.com>
2024-06-05 14:39:29 +0200

    text color

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,11 +1,11 @@
 <footer
-	class="bg-green-600 text-white text-center py-3 font-sans font"
+	class="bg-white text-black text-center py-3 font-sans font"
 	style="font-family: Arial, Helvetica, sans-serif;"
 >
 	<div class="flex justify-center items-center flex-row">
 		<div class="w-1/4 mt-5 mr-12">
-			<p class="font-bold text-white"><a href="/">Hogendijk Hoveniers</a></p>
-			<div style="height: 1px;" class="bg-white mt-1 w-5 mx-auto mb-0.5"></div>
+			<p class="font-bold text-black"><a href="/">Hogendijk Hoveniers</a></p>
+			<div style="height: 1px;" class="bg-black mt-1 w-5 mx-auto mb-0.5"></div>
 			<p>
 				Geen opdracht is te klein of te groot voor mijn hoveniersbedrijf! Alle soorten werkzaamheden
 				in en rond de tuin voer ik als hoveniersbedrijf met veel passie voor het vak uit.
@@ -16,14 +16,14 @@
 			</p>
 		</div>
 		<div class="mr-24">
-			<p class="font-bold text-white">Adres</p>
-			<div style="height: 1px;" class="bg-white mt-1 w-5 mx-auto mb-0.5"></div>
+			<p class="font-bold text-black">Adres</p>
+			<div style="height: 1px;" class="bg-black mt-1 w-5 mx-auto mb-0.5"></div>
 			<p class="text-sm">Merellaan 343</p>
 			<p class="text-sm">2903 GH Capelle a/d IJssel</p>
 		</div>
 		<div>
-			<p class="font-bold text-white">Contact</p>
-			<div style="height: 1px;" class="bg-white mt-1 w-5 mx-auto mb-0.5"></div>
+			<p class="font-bold text-black">Contact</p>
+			<div style="height: 1px;" class="bg-black mt-1 w-5 mx-auto mb-0.5"></div>
 			<p class="text-sm">Algemeen: 010 442 56 15</p>
 			<p class="text-sm">Albert Kasbergen: 06 470 20 960</p>
 			<p class="text-sm">E-mail: info@kasbergen-hoveniers.nl</p>
@@ -33,4 +33,3 @@
 		<p class="text-sm mt-10">&copy; 2024 Alle rechten voorbehouden aan Hogendijk Hoveniers</p>
 	</div>
 </footer>
-
