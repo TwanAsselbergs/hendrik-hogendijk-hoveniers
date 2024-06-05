777cb13bda9536186351a2c358d11e16ff019e63
Toaster5 <etiendenouden2006@gmail.com>
2024-06-05 14:01:04 +0200

    homepage

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,18 +2,9 @@
 	import logo from '../img/logo.png';
 </script>
 
-<header
-	style="background-color: white; color: grey; font-family: Arial, sans-serif;"
-	class="text-center py-3"
->
-	<div style="display: flex; justify-content: space-around; align-items: center;">
-		<h1 class="text-2xl">
-			<img src={logo} alt="Logo" style="height: 10rem; width: auto;" />
-		</h1>
-		<nav class="flex justify-around my-2">
-			<a href="+page.svelte" style="color: grey; font-weight: bold; margin-right: 1rem;">Home</a>
-			<a href="/about" style="color: grey; font-weight: bold; margin-right: 1rem;">Portfolio</a>
-			<a href="/contact" style="color: grey; font-weight: bold;">Contact</a>
-		</nav>
+<header class="bg-white fixed flex justify-center border-b border-black w-full h-32">
+	<div class="flex justify-between py-2 h-32 w-3/4 min-w-[800px]">
+		<div class="flex items-center h-full"><img class="h-full mr-10" src={logo} alt="" /></div>
+		<div class="flex items-center h-full">Test</div>
 	</div>
 </header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,8 @@
 <script>
-    import Header from "../components/Header.svelte";
-    import Footer from "../components/Footer.svelte"
+	import Header from '../components/Header.svelte';
+	import Footer from '../components/Footer.svelte';
 
-    import "../app.css"
+	import '../app.css';
 </script>
 
 <Header />
