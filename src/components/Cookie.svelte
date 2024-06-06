bb4594831ff7a10a88b923e6673274314a3bcc8e
Toaster5 <etiendenouden2006@gmail.com>
2024-06-06 09:24:40 +0200

    header and cookie

---
+++ src/components/Cookie.svelte
@@ -0,0 +1,15 @@
+
+
+<div class="fixed bottom-0 right-0 max-w-96 p-10">
+    <div class="flex flex-col w-full h-full bg-white border-2 rounded-xl p-2">
+        <h1 class="font-bold ml-3 my-2">Cookies</h1>
+        <p class="px-2 mb-3">
+            This website uses cookies to provide you with a more personalised and relevant browsing experience. <br>
+            <a class="text-green-500" href="#"> Read More...</a>
+        </p>
+        <div class="flex justify-evenly w-full gap-2">
+            <button class="bg-green-500 text-white border-2 rounded-md w-full py-2">Accept</button>
+            <button class="bg-white border-2 rounded-md w-full py-2">Reject</button>
+        </div>
+    </div>
+</div>
--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,7 +2,7 @@
 	import logo from '../img/logo.png';
 </script>
 
-<header class="bg-white fixed flex justify-center border-b border-black w-full h-32">
+<header class="bg-white fixed flex justify-center border-b-2 w-full h-32">
 	<div class="flex justify-between py-2 h-32 w-3/4 min-w-[800px]">
 		<div class="flex items-center h-full"><img class="h-full mr-10" src={logo} alt="" /></div>
 		<div class="flex items-center h-full">Test</div>
