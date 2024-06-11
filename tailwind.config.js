d892e1c40b328566cd3659664aed4ae5de3c7306
marwill206 <laptopmerhino22@hotmail.com>
2024-06-11 13:33:20 +0200

    refactor: Remove unnecessary code and update header background color to Hgreen

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte}'],
 	theme: {
-		extend: {},
-		colors: {
-			'white': '#fffffff',
-			'Hgreen': '#2A672D'
-		}
+		extend: {}
 	},
 	plugins: []
 };
