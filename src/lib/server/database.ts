add4e6ac39dfb55b176695e0d01d22ab9c53deca
TwanAsselbergs <twanasselbergs@outlook.com>
2024-06-12 11:54:48 +0200

    favicon

--- src/lib/server/database.ts
+++ src/lib/server/database.ts
@@ -1,6 +1,6 @@
-import { MongoClient, ObjectId } from "mongodb";
-import { env } from "$env/dynamic/private";
-import { fail } from "@sveltejs/kit";
+import { MongoClient, ObjectId } from 'mongodb';
+import { env } from '$env/dynamic/private';
+import { fail } from '@sveltejs/kit';
 
 const connection = await MongoClient.connect(env.DBURI);
 
@@ -10,30 +10,30 @@ const connection = await MongoClient.connect(env.DBURI);
  * @param {string} content
  */
 async function createReview(name: string, email: string, content: string) {
-    if (name === "" || email === "" || content === "") 
-        return fail(422, { msg: "Please fill in all fields" });
-        
-    let db = connection.db(env.DBNAME);
-    let review = {name, email, content};
-    
-    let res = await db.collection("reviews").insertOne(review);
+	if (name === '' || email === '' || content === '')
+		return fail(422, { msg: 'Please fill in all fields' });
 
-    console.log(res);
+	let db = connection.db(env.DBNAME);
+	let review = { name, email, content };
+
+	let res = await db.collection('reviews').insertOne(review);
+
+	console.log(res);
 }
 
 /**
  * @param {string} reviewId
  */
 async function deleteReview(reviewId: string) {
-    if (reviewId === "") return fail(422, { msg: "Please fill in all fields" });
+	if (reviewId === '') return fail(422, { msg: 'Please fill in all fields' });
 
-    let db = connection.db(env.DBNAME);
-    let review = {_id: new ObjectId(reviewId)};
-    
-    if (!await db.collection("review").findOne(review))
-        return fail(422, {msg: `Could not find review with id: ${reviewId}`});
+	let db = connection.db(env.DBNAME);
+	let review = { _id: new ObjectId(reviewId) };
 
-    await db.collection("reviews").deleteOne(review);
+	if (!(await db.collection('review').findOne(review)))
+		return fail(422, { msg: `Could not find review with id: ${reviewId}` });
+
+	await db.collection('reviews').deleteOne(review);
 }
 
 export { createReview, deleteReview };
