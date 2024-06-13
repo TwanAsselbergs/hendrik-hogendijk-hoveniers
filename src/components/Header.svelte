f153ee1d918482fd454d9396ee5579bcbcc6cb2c
TwanAsselbergs <twanasselbergs@outlook.com>
2024-06-13 21:14:57 +0200

    z-index

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -13,6 +13,7 @@
 <header
 	class={'bg-white fixed flex justify-center border-b-2 w-full transition-all ' +
 		(out ? 'h-20' : 'h-32')}
+	style="z-index: 1000;"
 >
 	<div class="flex justify-between py-2 h-full w-3/4 min-w-[800px]">
 		<a href="/" class="flex items-center h-full"><img class="h-full mr-10" src={logo} alt="" /></a>
