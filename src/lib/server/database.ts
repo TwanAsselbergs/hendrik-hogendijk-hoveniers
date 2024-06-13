e6b759235d0cfc27b26e2edf40623d0d4e6e8a67
marwill206 <laptopmerhino22@hotmail.com>
2024-06-13 11:51:46 +0200

    refactor: Update database.ts and +page.svelte

--- src/lib/server/database.ts
+++ src/lib/server/database.ts
@@ -33,16 +33,20 @@ async function deleteReview(reviewId: string) {
 	if (!(await db.collection('review').findOne(review)))
 		return fail(422, { msg: `Could not find review with id: ${reviewId}` });
 
-    await db.collection("reviews").deleteOne(review);
+	await db.collection('reviews').deleteOne(review);
 }
 
-async function readReview (){
-let db =connection.db(env.DBNAME);
-let result = await db.collection("reviews").find().toArray();
-return result;
+async function readReview() {
+	let db = connection.db(env.DBNAME);
+	let result = await db.collection('reviews').find().toArray();
+	return result;
 }
 
-export { createReview, deleteReview, readReview };
-export default { createReview, deleteReview, readReview };
+async function generalRead() {
+	let db = connection.db(env.DBNAME);
+	let result = await db.collection('general').find().toArray();
+	return result;
+}
 
-
+export { createReview, deleteReview, readReview, generalRead };
+export default { createReview, deleteReview, readReview, generalRead };
