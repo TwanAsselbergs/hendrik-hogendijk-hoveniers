6f06157c4bf87bdbb00ec59d4155527e6f744e04
Toaster5 <etiendenouden2006@gmail.com>
2024-06-17 12:26:31 +0200

    info & pt

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,47 +16,53 @@
 	}
 </script>
 
-<main class="flex flex-col min-h-screen pt-12">
+<main class="flex flex-col min-h-screen pt-12 tall:pt-0">
 	<section class="relative flex justify-center items-center w-full h-screen md:px-24">
 		<div
 			bind:this={scrollContent}
 			on:scroll={(e) => (x = e.target.scrollLeft / (e.target.scrollWidth - e.target.clientWidth))}
 			class="px-10 md:px-36 py-10 flex items-center flex-row overflow-x-scroll scrollbar-hide snap-x snap-mandatory gap-10 h-[600px] min-w-full"
 		>
-			<div class="snap-center min-w-[460px] h-full py-10">
-				<h1 class="pl-3 text-xl font-bold">Hendrik Hogendijk Hoveniers</h1>
-				<p class="pt-8 ml-3">
-					Geen opdracht is te klein of te groot voor mijn hoveniersbedrijf! Alle soorten
-					werkzaamheden in en rond de tuin voer ik als hoveniersbedrijf met veel passie voor het vak
-					uit. Zo kunt u er dan ook blindelings vanuit gaan dat uw tuin naar wens wordt opgeleverd,
-					eerder zal ik namelijk niet vertrekken. Vakmanschap, kwaliteit en service, dat is het
-					handelsmerk van Hogendijk Hoveniers.
-				</p>
+			<div class="snap-center flex min-w-[920px] h-full py-10">
+				<div class="h-full w-full">
+					<h1 class="pl-3 text-xl font-bold">Hendrik Hogendijk Hoveniers</h1>
+					<p class="pt-8 ml-3">
+						Geen opdracht is te klein of te groot voor mijn hoveniersbedrijf! Alle soorten
+						werkzaamheden in en rond de tuin voer ik als hoveniersbedrijf met veel passie voor het vak
+						uit. Zo kunt u er dan ook blindelings vanuit gaan dat uw tuin naar wens wordt opgeleverd,
+						eerder zal ik namelijk niet vertrekken. Vakmanschap, kwaliteit en service, dat is het
+						handelsmerk van Hogendijk Hoveniers.
+					</p>
+				</div>
+				<div class="bg-black w-full h-full rounded-lg overflow-hidden">
+					<img class="w-full h-full object-cover" src={tuin1} alt="Tuin 1" />
+				</div>
 			</div>
-			<div class="snap-center bg-black min-w-[460px] h-full rounded-lg overflow-hidden">
-				<img class="w-full h-full object-cover" src={tuin1} alt="Tuin 1" />
+			<div class="snap-center flex min-w-[920px] h-full py-10">
+				<div class="w-full h-full py-10">
+					<h1 class="pl-3 text-xl font-bold">Example text</h1>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis veniam laboriosam
+						optio aspernatur repudiandae assumenda beatae rem exercitationem minima enim. Aut,
+						recusandae dolores. Fuga debitis, similique voluptas facere rerum sunt!
+					</p>
+				</div>
+				<div class="bg-black min-w-[460px] h-full rounded-lg overflow-hidden">
+					<img class="w-full h-full object-cover" src={tuin2} alt="Tuin 2" />
+				</div>
 			</div>
-			<div class="snap-center min-w-[460px] h-full py-10">
-				<h1 class="pl-3 text-xl font-bold">Example text</h1>
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis veniam laboriosam
-					optio aspernatur repudiandae assumenda beatae rem exercitationem minima enim. Aut,
-					recusandae dolores. Fuga debitis, similique voluptas facere rerum sunt!
-				</p>
-			</div>
-			<div class="snap-center bg-black min-w-[460px] h-full rounded-lg overflow-hidden">
-				<img class="w-full h-full object-cover" src={tuin2} alt="Tuin 2" />
-			</div>
-			<div class="snap-center min-w-[460px] h-full py-10">
-				<h1 class="pl-3 text-xl font-bold">Example text</h1>
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis veniam laboriosam
-					optio aspernatur repudiandae assumenda beatae rem exercitationem minima enim. Aut,
-					recusandae dolores. Fuga debitis, similique voluptas facere rerum sunt!
-				</p>
-			</div>
-			<div class="snap-center bg-black min-w-[460px] h-full">
-				<img class="w-full h-full object-cover" src={tuin3} alt="Tuin 3" />
+			<div class="snap-center flex min-w-[920px] h-full py-10">
+				<div class="w-full h-full py-10">
+					<h1 class="pl-3 text-xl font-bold">Example text</h1>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis veniam laboriosam
+						optio aspernatur repudiandae assumenda beatae rem exercitationem minima enim. Aut,
+						recusandae dolores. Fuga debitis, similique voluptas facere rerum sunt!
+					</p>
+				</div>
+				<div class="bg-black min-w-[460px] h-full rounded-lg overflow-hidden">
+					<img class="w-full h-full object-cover" src={tuin3} alt="Tuin 3" />
+				</div>
 			</div>
 		</div>
 		<div class="absolute bg-gray-200 left-[50%] -translate-x-[50%] w-1/2 md:w-1/3 h-1 bottom-[10%]">
