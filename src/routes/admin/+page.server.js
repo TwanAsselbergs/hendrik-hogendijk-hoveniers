ba0e9bcd1080c23b9c8c69d2a5b75cccee40327b
marwill206 <laptopmerhino22@hotmail.com>
2024-06-20 10:34:21 +0200

    chore: Refactor admin page server code and update admin page layout
    This commit refactors the server code for the admin page and updates the layout of the admin page. The server code has been modified to include a new data source and serialize the data. The layout of the admin page has been improved by adding styling and organizing the data in separate sections.

--- src/routes/admin/+page.server.js
+++ src/routes/admin/+page.server.js
@@ -10,20 +10,24 @@ import { serialize } from 'v8';
 export const load = async () => {
 	const data = await readGeneral();
 	const dataH = await readHendrik();
+    const dataR = await readReview();
+    const serializableDataR=data.map((item)=> {
+        
+    } );
 	const serializableData = data.map((item) => item.text);
 	const serializableDataH = dataH.map((item) => ({
-        Fname: item.name.fName,
-        Lname: item.name.lName,
-        Number: item.number,
-        Email: item.email,
-        City: item.address.city,
-        Street: item.address.street,
-        HNumber: item.address.hNumber,
-        PostalCode: item.address.postalCode,
-        Facebook: item.socials.facebook,
-        Instagram: item.socials.instagram
-     }));
-	// console.log(JSON.stringify(serializableDataH, null, 2));
+		Fname: item.name.fName,
+		Lname: item.name.lName,
+		Number: item.number,
+		Email: item.email,
+		City: item.address.city,
+		Street: item.address.street,
+		HNumber: item.address.hNumber,
+		PostalCode: item.address.postalCode,
+		Facebook: item.socials.facebook,
+		Instagram: item.socials.instagram
+	}));
+    
 	return { props: { data: serializableData, dataH: serializableDataH } };
 };
 
--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,17 +1,42 @@
 <script>
 	import { json } from '@sveltejs/kit';
-    export let data;
+	export let data;
 </script>
- <main class="flex justify-center items-center h-screen">
-    <div class="flex">
-        <div class=""></div>
-        <div></div>
-    </div>
-    <div class="flex row"> 
-        {#each data.props.dataH as item (item._id)}
-        <div class="">{JSON.stringify(item)}</div>
-     {/each}
-        <div></div>
-    </div>
-    <div></div>
-</main> 
+
+<main class="flex justify-center items-center h-screen">
+	<div class="flex">
+		<div class=""></div>
+		<div></div>
+	</div>
+	<div class="flex row gap-10">
+		<div class="w-80 border border-green-400 border-2 rounded-md p-2">
+			{#each data.props.data as item (item)}
+				<div class="">{item}</div>
+			{/each}
+			<button class="mt-2 bg-green-600 text-white px-6 py-2 rounded-md hover:bg-green-700">
+                edit
+            </button>
+		</div>
+		<div class=" border border-green-400 border-2 rounded-md p-2">
+			{#each data.props.dataH as item (item)}
+				<div>
+					<h2>{item.Fname} {item.Lname}</h2>
+					<p>{item.Number}</p>
+					<p>{item.Email}</p>
+					<p>{item.City}</p>
+					<p>{item.Street} {item.HNumber}</p>
+					<p>{item.PostalCode}</p>
+					<p>{item.Instagram}</p>
+					<p>{item.Facebook}</p>
+				</div>
+			{/each}
+            <button class="mt-2 bg-green-600 text-white px-6 py-2 rounded-md hover:bg-green-700">
+                edit
+            </button>
+		</div>
+
+        
+	</div>
+
+	<div></div>
+</main>
