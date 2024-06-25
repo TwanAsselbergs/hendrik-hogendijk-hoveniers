3ddb50c3361ad83d0372d11cbcb5aa7d6cbe0f07
marwill206 <laptopmerhino22@hotmail.com>
2024-06-25 16:10:58 +0200

    change

---
+++ src/routes/update/+page.server.js
@@ -0,0 +1,34 @@
+import {
+	createReview,
+	deleteReview,
+	readGeneral,
+	readReviews,
+	readHendrik
+} from '$lib/server/database';
+import { serialize } from 'v8';
+
+export const load = async () => {
+	const data = await readGeneral();
+	const dataH = await readHendrik();
+    const dataR = await readReviews();
+	const serializableDataR = dataR.map((item) => ({
+		review: item.review,
+		name: item.name
+	}));
+	const serializableData = data.map((item) => item.text);
+	const serializableDataH = dataH.map((item) => ({
+		Fname: item.name.fName,
+		Lname: item.name.lName,
+		Number: item.number,
+		Email: item.email,
+		City: item.address.city,
+		Street: item.address.street,
+		HNumber: item.address.hNumber,
+		PostalCode: item.address.postalCode,
+		Facebook: item.socials.facebook,
+		Instagram: item.socials.instagram
+	}));
+  
+	return { props: { data: serializableData, dataH: serializableDataH, dataR: serializableDataR } };
+};
+
