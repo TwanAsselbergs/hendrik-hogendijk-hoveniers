d8e30583765e4dcf133bd8de528615a53174d863
marwill206 <laptopmerhino22@hotmail.com>
2024-06-27 11:50:19 +0200

    chore: Update Footer component layout and styling

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -15,38 +15,40 @@
 				Hoveniers.
 			</p>
 		</div>
-		<div class="w-full sm:w-auto sm:mr-24 mt-5 sm:mt-0">
-			<p class="font-bold text-gray-300">Adres</p>
-			<div style="height: 1px;" class="bg-black mt-1 w-5 mx-auto mb-0.5"></div>
-			<p class="text-sm">Hoefstraat 42</p>
-			<p class="text-sm">7143HH Huis Ter Heide</p>
-		</div>
-		<div class="w-full sm:w-auto mt-5 sm:mt-12">
-			<p class="font-bold text-gray-300">Contact</p>
-			<div style="height: 1px;" class="bg-black mt-1 w-5 mx-auto mb-1"></div>
-			<p class="text-sm">
-				Telefoon: <a href="tel:0612345678" class="text-green-700 hover:underline">06 12345678</a>
-			</p>
-			<p class="text-sm">
-				E-mail:
-				<a href="mailto:hendrikhogendijkhoveniers@gmail.com" class="text-green-700 hover:underline">
-					hendrikhogendijkhoveniers@gmail.com
-				</a>
-			</p>
-			<div class="flex justify-center mt-3">
-				<a href="https://www.facebook.com/profile.php?id=61561673542984" target="_blank">
-					<img src="src/img/facebook.png" alt="Facebook" width="24" height="24" class="ml-1 mr-2" />
-				</a>
-				<a href="https://www.instagram.com/hendrikhogendijk" target="_blank">
-					<img src="src/img/instagram.png" alt="Instagram" width="24" height="24" />
-				</a>
+		
+			
+		 <div class="w-full sm:w-auto sm:mr-24 mt-5 sm:mt-0">
+			 <p class="font-bold text-gray-300">Adres</p>
+			 <div style="height: 1px;" class="bg-black mt-1 w-5 mx-auto mb-0.5"></div>
+			 <p class="text-sm">Hoefstraat 42</p>
+			 <p class="text-sm">7143HH Huis Ter Heide</p>
+			</div>
+			<div class="w-full sm:w-auto mt-5 sm:mt-12">
+				<p class="font-bold text-gray-300">Contact</p>
+				<div style="height: 1px;" class="bg-black mt-1 w-5 mx-auto mb-1"></div>
+				<p class="text-sm">
+					Telefoon: <a href="tel:0612345678" class="text-green-700 hover:underline">06 12345678</a>
+				</p>
+				<p class="text-sm">
+					E-mail:
+					<a href="mailto:hendrikhogendijkhoveniers@gmail.com" class="text-green-700 hover:underline">
+						hendrikhogendijkhoveniers@gmail.com
+					</a>
+				</p>
+				<div class="flex justify-center mt-3">
+					<a href="https://www.facebook.com/profile.php?id=61561673542984" target="_blank">
+						<img src="src/img/facebook.png" alt="Facebook" width="24" height="24" class="ml-1 mr-2" />
+					</a>
+					<a href="https://www.instagram.com/hendrikhogendijk" target="_blank">
+						<img src="src/img/instagram.png" alt="Instagram" width="24" height="24" />
+					</a>
+				</div>
 			</div>
 		</div>
-	</div>
-	<div
+		<div
 		class="flex flex-col items-center justify-center w-full h-16 pb-20"
 		style="background-color: rgb(51,50,50);"
-	>
+		>
 		<hr class="w-3/4 border-gray-700 pb-10" />
 		<p class="text-sm">&copy; 2024 Alle rechten voorbehouden aan Hendrik Hogendijk Hoveniers</p>
 	</div>
