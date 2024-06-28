57e1cbb7e27f8f527ee459e0bf5a327330b83ba3
TwanAsselbergs <twanasselbergs@outlook.com>
2024-06-28 09:39:05 +0200

    push

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -3,6 +3,7 @@
 	import Logo from '../../components/Logo.svelte';
 	import { goto } from '$app/navigation';
 	import { onMount } from 'svelte';
+	import { link } from '$app/navigation';
 
 	export let data;
 
@@ -35,29 +36,36 @@
 						Bewerk
 					</button>
 				</form> -->
-				<a
-					href="/admin/update/{item.id}"
-					class=" w-2/6 mt-2 bg-green-600 text-white px-6 py-2 rounded-md hover:bg-green-700"
-				>
-					Bewerk
-				</a>
-			</div>
-		{/each}
-		{#each data.props.dataH as item (item)}
-		<div class="w-82 border border-green-400 border-2 rounded-md p-2 flex flex-col justify-between">
-				<div class=" flex flex-col gap-2">
-					<h2>{item.Fname} {item.Lname}</h2>
-					<p>{item.Number}</p>
-					<p>{item.Email}</p>
-					<p>{item.City}</p>
-					<p>{item.Street} {item.HNumber}</p>
-					<p>{item.PostalCode}</p>
-					<p>{item.Instagram}</p>
-					<p>{item.Facebook}</p>
+					<a
+						href="/admin/update/{item.id}"
+						use:link
+						class="w-2/6 mt-2 bg-green-600 text-white px-6 py-2 rounded-md hover:bg-green-700"
+					>
+						Bewerk
+					</a>
 				</div>
-				<a href="/admin/updateH/{item.id}" class="mt-2 bg-green-600 text-white px-6 py-2 rounded-md hover:bg-green-700 w-2/6">
-					Bewerk
-				</a>
+			{/each}
+			{#each data.props.dataH as item (item)}
+				<div
+					class="w-82 border border-green-400 border-2 rounded-md p-2 flex flex-col justify-between"
+				>
+					<div class=" flex flex-col gap-2">
+						<h2>{item.Fname} {item.Lname}</h2>
+						<p>{item.Number}</p>
+						<p>{item.Email}</p>
+						<p>{item.City}</p>
+						<p>{item.Street} {item.HNumber}</p>
+						<p>{item.PostalCode}</p>
+						<p>{item.Instagram}</p>
+						<p>{item.Facebook}</p>
+					</div>
+					<a
+						href="/admin/updateH/{item.id}"
+						use:link
+						class="mt-2 bg-green-600 text-white px-6 py-2 rounded-md hover:bg-green-700 w-2/6"
+					>
+						Bewerk
+					</a>
 				</div>
 			{/each}
 		</div>
